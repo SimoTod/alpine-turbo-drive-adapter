20eaa6cf75d64c5b6847d26c35f331b349ddd98f
Simone Todaro <simone.todaro@gear4music.com>
2023-09-17 21:25:42 +0100

    chore: migrate test files

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: false,
+  e2e: {
+    setupNodeEvents (on, config) {},
+    supportFile: 'tests/bootstrap.js',
+    specPattern: 'tests/integration/**/*.cy.{js,jsx,ts,tsx}'
+  }
+})
